eab90a0ef16fe97316b61d807a97c32745a4c047
Delta_Zheng <Delta_Zheng@wistronits.com>
2022-10-26 13:41:57 +0800

    init

---
+++ src/app.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { ApiProperty } from '@nestjs/swagger';
+import { AppService } from './app.service';
+
+@Controller('/user')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('/name')
+  @ApiProperty({ description: '用戶的名字' })
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}

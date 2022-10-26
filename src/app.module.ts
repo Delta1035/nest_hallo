0ce7967e72deba871ba527f939fc91a25a7f55ea
Delta_Zheng <Delta_Zheng@wistronits.com>
2022-10-26 13:58:37 +0800

    调整结构

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controller/app.controller';
+import { AppService } from './service/app.service';
+import { LoginController } from './controller/login/login.controller';
+import { LoginService } from './service/login/login.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, LoginController],
+  providers: [AppService, LoginService],
 })
 export class AppModule {}
